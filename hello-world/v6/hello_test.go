d363efc542175ceff9f66dfe46e3f3be6ae701be
yerim <dpfla725@naver.com>
2023-10-15 18:15:01 +0900

    각 케이스별 테스트 추가 - 스페인,프랑스

--- hello-world/v6/hello_test.go
+++ hello-world/v6/hello_test.go
@@ -4,15 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	// todo -> 각 나라별 테스트 케이스 짜기
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Eldie", "Spanish")
+
+	t.Run("to a person", func(t *testing.T) {
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Eldie", "Spanish")
+		want := "Hola, Eldie"
 		assertCorrectMessage(t, got, want)
 
 	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("", "")
-		want := "Hello, World"
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Lauren", french)
+		want := "Bonjour, Lauren"
 		assertCorrectMessage(t, got, want)
 	})
 }
