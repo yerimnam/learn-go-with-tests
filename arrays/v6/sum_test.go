7ce42f1598357aaa484f7d1d78dfac5fcae1096d
yrnam <yrnam@dozn.co.kr>
2023-10-24 13:48:27 +0900

    슬라이스 활용한 테스트 코드 작성

---
+++ arrays/v6/sum_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("colleciton of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given,%v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	})
+
+}
