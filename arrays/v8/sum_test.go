395ceb7dd793e37a17fc96b70319c2935c3f65fb
yrnam <yrnam@dozn.co.kr>
2023-10-30 17:21:48 +0900

    checkSum함수로 테스트하도록 refactor

---
+++ arrays/v8/sum_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper() //호출된 함수를 표시해준다.
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+
+	})
+
+	t.Run("safetly sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
+}
